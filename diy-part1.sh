9d7e229f24cfc07bde7b3a2ebbe79d746e0ac495
Wyatt323 <71224625+Wyatt323@users.noreply.github.com>
2023-04-14 18:33:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,8 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 #加入mcat主题
 echo 'src-git mcat https://github.com/Wyatt323/luci-theme-mcat' >>feeds.conf.default
+echo 'src-git mcat https://github.com/coolsnowwolf/luci/tree/master/themes' >>feeds.conf.default
+
 
 #加入kenzok8大佬的软件包
 #echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
