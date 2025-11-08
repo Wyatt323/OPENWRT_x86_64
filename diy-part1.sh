c87f378fb5b0213d83271e3563036764d7524e21
Wyatt323 <71224625+Wyatt323@users.noreply.github.com>
2025-11-08 22:25:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,11 +19,12 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #加入mcat主题
 echo 'src-git mcat https://github.com/Wyatt323/luci-theme-mcat' >>feeds.conf.default
 
-
 #加入kenzok8大佬的软件包
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
+#加入MosDNS软件
+echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 
 #加入 DDNSTO到feeds配置文件里
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
